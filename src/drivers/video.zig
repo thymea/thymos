9339291bda90059f54f02908b9f2a43db8a8c680
Voxi0 <alif200099@gmail.com>
2025-09-10 18:10:53 +0100

    chore: cleaning up video driver
    just cleaning things up and improving things wherever possible. i'm
    currently aiming to avoid type casting and such as much as possible. i
    feel like i messed around with the zig type system really badly and am
    doing things in horrible ways.

--- src/drivers/video.zig
+++ src/drivers/video.zig
@@ -15,8 +15,10 @@ pub export var ssfn_dst: g.c.ssfn_buf_t = undefined;
 
 // Framebuffer
 export var fbRequest: g.limine.FramebufferRequest linksection(".limine_requests") = .{};
-var fb: ?*g.limine.Framebuffer = null;
+var fb: *g.limine.Framebuffer = undefined;
 var pitchInPixels: u32 = 0;
+
+// Text cursor
 
 // Utility/Helper functions
 pub fn strToHex(colorStr: [*]u8) u32 {
@@ -53,14 +55,14 @@ pub fn init(backgroundColor: u32, foregroundColor: u32) void {
     if (fbRequest.response) |response| {
         // Get the framebuffer
         fb = response.getFramebuffers()[0];
-        pitchInPixels = @intCast(fb.?.pitch / @sizeOf(u32));
+        pitchInPixels = @intCast(fb.pitch / @sizeOf(u32));
 
         // Initialize SSFN - Text renderer
         g.c.ssfn_src = @ptrCast(@constCast(&FONT[0]));
-        g.c.ssfn_dst.ptr = @ptrCast(fb.?.address);
-        g.c.ssfn_dst.w = @intCast(fb.?.width);
-        g.c.ssfn_dst.h = @intCast(fb.?.height);
-        g.c.ssfn_dst.p = @intCast(fb.?.pitch);
+        g.c.ssfn_dst.ptr = @ptrCast(fb.address);
+        g.c.ssfn_dst.w = @intCast(fb.width);
+        g.c.ssfn_dst.h = @intCast(fb.height);
+        g.c.ssfn_dst.p = @intCast(fb.pitch);
         g.c.ssfn_dst.x = 0;
         g.c.ssfn_dst.y = 0;
         g.c.ssfn_dst.bg = backgroundColor;
@@ -70,7 +72,7 @@ pub fn init(backgroundColor: u32, foregroundColor: u32) void {
 
 // Clear the screen and reset cursor position
 pub fn resetScreen() void {
-    drawFilledRect(0, 0, @intCast(fb.?.width), @intCast(fb.?.height), g.c.ssfn_dst.bg);
+    drawFilledRect(0, 0, @intCast(fb.width), @intCast(fb.height), g.c.ssfn_dst.bg);
     g.c.ssfn_dst.x = 0;
     g.c.ssfn_dst.y = 0;
 }
@@ -84,11 +86,11 @@ pub fn drawPixel(xpos: u64, ypos: u64, color: u32) void {
 pub fn drawFilledRect(xpos: u64, ypos: u64, width: u32, height: u32, color: u32) void {
     // Clamp to valid range
     if (xpos < 0 or ypos < 0) return;
-    if ((xpos + width) > fb.?.width) return;
-    if ((ypos + height) > fb.?.height) return;
+    if ((xpos + width) > fb.width) return;
+    if ((ypos + height) > fb.height) return;
 
     // Calculate the start position to start drawing in and draw the rectangle
-    var rowPtr: [*]u32 = @as([*]u32, @ptrCast(@alignCast(fb.?.address))) + ypos * pitchInPixels + xpos;
+    var rowPtr: [*]u32 = @as([*]u32, @ptrCast(@alignCast(fb.address))) + ypos * pitchInPixels + xpos;
     var pixelPtr: [*]u32 = rowPtr;
     for (0..height) |_| {
         // Fill the entire row with color
@@ -109,14 +111,14 @@ export fn _putchar(char: u8) void {
         // Newline
         '\n' => {
             g.c.ssfn_dst.x = 0;
-            if ((g.c.ssfn_dst.y + GLYPH_HEIGHT) > fb.?.height) {
+            if ((g.c.ssfn_dst.y + GLYPH_HEIGHT) > fb.height) {
                 resetScreen();
             } else g.c.ssfn_dst.y += GLYPH_HEIGHT;
         },
 
         // Tab
         '\t' => {
-            if ((g.c.ssfn_dst.x + (GLYPH_WIDTH * TAB_SPACES)) > fb.?.width) {
+            if ((g.c.ssfn_dst.x + (GLYPH_WIDTH * TAB_SPACES)) > fb.width) {
                 resetScreen();
             } else {
                 for (0..4) |_|
@@ -131,7 +133,7 @@ export fn _putchar(char: u8) void {
                 g.c.ssfn_dst.x -= GLYPH_WIDTH;
             } else if (g.c.ssfn_dst.y >= GLYPH_HEIGHT) {
                 g.c.ssfn_dst.y -= GLYPH_HEIGHT;
-                g.c.ssfn_dst.x = @intCast(fb.?.width - GLYPH_WIDTH);
+                g.c.ssfn_dst.x = @intCast(fb.width - GLYPH_WIDTH);
             } else return;
 
             // Draw over the character to "erase it"
@@ -140,9 +142,11 @@ export fn _putchar(char: u8) void {
 
         // Normal character
         else => {
-            if ((g.c.ssfn_dst.x + GLYPH_WIDTH) > fb.?.width) _putchar('\n');
-            if ((g.c.ssfn_dst.y + GLYPH_HEIGHT) > fb.?.height) resetScreen();
+            if ((g.c.ssfn_dst.x + GLYPH_WIDTH) > fb.width) _putchar('\n');
+            if ((g.c.ssfn_dst.y + GLYPH_HEIGHT) > fb.height) resetScreen();
             _ = g.c.ssfn_putc(char);
         },
     }
+
+    drawFilledRect(@intCast(ssfn_dst.x), @intCast(ssfn_dst.y), 1, GLYPH_HEIGHT, ssfn_dst.fg);
 }
