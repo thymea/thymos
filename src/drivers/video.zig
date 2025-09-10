2e2dae1bd5137e4248141a8e09ecefe50f233ef7
Voxi0 <alif200099@gmail.com>
2025-09-10 18:45:50 +0100

    chore: video driver further cleaning
    just making sure to avoid using unncessary types and avoiding `g.c.` or
    whatever prefixes whenever possible. it's surprising how overcomplicated
    i made my code, like a bunch of variables have overly complicated types
    that can be replaced with something far simpler with the help of the
    `undefined` keyword whenever i need an uninitialized variable. for
    example, it serves as a placeholder value for the `fb` variable which
    holds the variable in global scope so all functions in the video driver
    can use it. however, it's not actually initialized with the correct
    framebuffer until the init function is called of course, so i use
    `undefined` as a placeholder until i can set it to point at the correct
    framebuffer.

--- src/drivers/video.zig
+++ src/drivers/video.zig
@@ -3,22 +3,20 @@ const g = @import("../index.zig");
 // Constants
 // Font
 const FONT = @embedFile("../fonts/unifont.sfn");
-const GLYPH_WIDTH: u8 = 8;
-const GLYPH_HEIGHT: u8 = 16;
+pub const GLYPH_WIDTH: u8 = 8;
+pub const GLYPH_HEIGHT: u8 = 16;
 
 // Number of spaces in a tab character
 const TAB_SPACES: u8 = 4;
 
 // SSFN
-pub export var ssfn_src: ?*g.c.ssfn_font_t = null;
+pub export var ssfn_src: *g.c.ssfn_font_t = undefined;
 pub export var ssfn_dst: g.c.ssfn_buf_t = undefined;
 
 // Framebuffer
 export var fbRequest: g.limine.FramebufferRequest linksection(".limine_requests") = .{};
 var fb: *g.limine.Framebuffer = undefined;
 var pitchInPixels: u32 = 0;
-
-// Text cursor
 
 // Utility/Helper functions
 pub fn strToHex(colorStr: [*]u8) u32 {
@@ -42,6 +40,12 @@ pub fn strToHex(colorStr: [*]u8) u32 {
 pub fn rgbToHex(red: u32, green: u32, blue: u32) u32 {
     return ((red & 0xff) << 16) + ((green & 0xff) << 8) + (blue & 0xff);
 }
+pub fn getCursorX() c_int {
+    return ssfn_dst.x;
+}
+pub fn getCursorY() c_int {
+    return ssfn_dst.y;
+}
 pub fn setBgColor(color: u32) void {
     ssfn_dst.bg = color;
 }
@@ -58,29 +62,29 @@ pub fn init(backgroundColor: u32, foregroundColor: u32) void {
         pitchInPixels = @intCast(fb.pitch / @sizeOf(u32));
 
         // Initialize SSFN - Text renderer
-        g.c.ssfn_src = @ptrCast(@constCast(&FONT[0]));
-        g.c.ssfn_dst.ptr = @ptrCast(fb.address);
-        g.c.ssfn_dst.w = @intCast(fb.width);
-        g.c.ssfn_dst.h = @intCast(fb.height);
-        g.c.ssfn_dst.p = @intCast(fb.pitch);
-        g.c.ssfn_dst.x = 0;
-        g.c.ssfn_dst.y = 0;
-        g.c.ssfn_dst.bg = backgroundColor;
-        g.c.ssfn_dst.fg = foregroundColor;
+        ssfn_src = @ptrCast(@constCast(&FONT[0]));
+        ssfn_dst.ptr = @ptrCast(fb.address);
+        ssfn_dst.w = @intCast(fb.width);
+        ssfn_dst.h = @intCast(fb.height);
+        ssfn_dst.p = @intCast(fb.pitch);
+        ssfn_dst.x = 0;
+        ssfn_dst.y = 0;
+        ssfn_dst.bg = backgroundColor;
+        ssfn_dst.fg = foregroundColor;
     } else @panic("No framebuffer available");
 }
 
 // Clear the screen and reset cursor position
 pub fn resetScreen() void {
-    drawFilledRect(0, 0, @intCast(fb.width), @intCast(fb.height), g.c.ssfn_dst.bg);
-    g.c.ssfn_dst.x = 0;
-    g.c.ssfn_dst.y = 0;
+    drawFilledRect(0, 0, @intCast(fb.width), @intCast(fb.height), ssfn_dst.bg);
+    ssfn_dst.x = 0;
+    ssfn_dst.y = 0;
 }
 
 // Drawing
 pub fn drawPixel(xpos: u64, ypos: u64, color: u32) void {
     // Calculate the absolute pixel position and color it
-    const pixels: [*]u32 = @ptrCast(@alignCast(fb.?.address));
+    const pixels: [*]u32 = @ptrCast(@alignCast(fb.address));
     pixels[ypos * pitchInPixels + xpos] = color;
 }
 pub fn drawFilledRect(xpos: u64, ypos: u64, width: u32, height: u32, color: u32) void {
@@ -110,15 +114,15 @@ export fn _putchar(char: u8) void {
     switch (char) {
         // Newline
         '\n' => {
-            g.c.ssfn_dst.x = 0;
-            if ((g.c.ssfn_dst.y + GLYPH_HEIGHT) > fb.height) {
+            ssfn_dst.x = 0;
+            if ((ssfn_dst.y + GLYPH_HEIGHT) > fb.height) {
                 resetScreen();
-            } else g.c.ssfn_dst.y += GLYPH_HEIGHT;
+            } else ssfn_dst.y += GLYPH_HEIGHT;
         },
 
         // Tab
         '\t' => {
-            if ((g.c.ssfn_dst.x + (GLYPH_WIDTH * TAB_SPACES)) > fb.width) {
+            if ((ssfn_dst.x + (GLYPH_WIDTH * TAB_SPACES)) > fb.width) {
                 resetScreen();
             } else {
                 for (0..4) |_|
@@ -129,21 +133,27 @@ export fn _putchar(char: u8) void {
         // Backspace
         0x08 => {
             // Move cursor back to previous character
-            if (g.c.ssfn_dst.x >= GLYPH_WIDTH) {
-                g.c.ssfn_dst.x -= GLYPH_WIDTH;
-            } else if (g.c.ssfn_dst.y >= GLYPH_HEIGHT) {
-                g.c.ssfn_dst.y -= GLYPH_HEIGHT;
-                g.c.ssfn_dst.x = @intCast(fb.width - GLYPH_WIDTH);
+            if (ssfn_dst.x >= GLYPH_WIDTH) {
+                ssfn_dst.x -= GLYPH_WIDTH;
+            } else if (ssfn_dst.y >= GLYPH_HEIGHT) {
+                ssfn_dst.y -= GLYPH_HEIGHT;
+                ssfn_dst.x = @intCast(fb.width - GLYPH_WIDTH);
             } else return;
 
-            // Draw over the character to "erase it"
-            drawFilledRect(@intCast(g.c.ssfn_dst.x), @intCast(g.c.ssfn_dst.y), GLYPH_WIDTH, GLYPH_HEIGHT, g.c.ssfn_dst.bg);
+            // Draw over the previous character to "erase" it
+            drawFilledRect(
+                @intCast(ssfn_dst.x),
+                @intCast(ssfn_dst.y),
+                GLYPH_WIDTH,
+                GLYPH_HEIGHT,
+                ssfn_dst.bg,
+            );
         },
 
         // Normal character
         else => {
-            if ((g.c.ssfn_dst.x + GLYPH_WIDTH) > fb.width) _putchar('\n');
-            if ((g.c.ssfn_dst.y + GLYPH_HEIGHT) > fb.height) resetScreen();
+            if ((ssfn_dst.x + GLYPH_WIDTH) > fb.width) _putchar('\n');
+            if ((ssfn_dst.y + GLYPH_HEIGHT) > fb.height) resetScreen();
             _ = g.c.ssfn_putc(char);
         },
     }
